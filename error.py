d12b0db9e443a28ac680914a6b63ac97471bc647
PAN Guolin <Guolin.Pan@dell.com>
2018-02-12 00:16:48 +0800

    add asserts

--- error.py
+++ error.py
@@ -19,7 +19,110 @@ def is_success(err):
     return err == err_success
 
 
+# Assert
+
+def assert_success(err):
+    assert(is_success(err))
+
+
+def assert_failed(err):
+    assert(not is_success(err))
+
+
+def assert_true(condition):
+    assert(condition)
+
+
+def assert_false(condition):
+    assert(not (condition))
+
+
+def assert_is_instance(instance, class_or_type):
+    assert(isinstance(instance, class_or_type))
+
+
+def assert_is_not_instance(instance, class_or_type):
+    assert(not isinstance(instance, class_or_type))
+
+
+def assert_is_subclass(child, parent):
+    assert(issubclass(child, parent))
+
+
+def assert_is_not_subclass(child, parent):
+    assert(not issubclass(child, parent))
+
+
+def assert_is_none(instance):
+    assert(instance is None)
+
+
+def assert_is_not_none(instance):
+    assert(instance is not None)
+
+
+def assert_is_list(instance):
+    assert(type(instance) == list)
+
+
+def assert_is_dict(instance):
+    assert(type(instance) == dict)
+
+
+def assert_is_tuple(instance):
+    assert(type(instance) == tuple)
+
+
+def assert_is_int(instance):
+    assert(type(instance) == int)
+
+
+def assert_is_str(instance):
+    assert(type(instance) == str)
+
+
+def assert_is_bool(instance):
+    assert(type(instance) == bool)
+
+
+def assert_is_float(instance):
+    assert(type(instance) == float)
+
+
+def assert_is_same(instance1, instance2):
+    assert(instance1 is instance2)
+
+
+def assert_is_not_same(instance1, instance2):
+    assert(instance1 is not instance2)
+
+
+def assert_int_equal(instance1, instance1):
+    assert_is_int(instance1)
+    assert_is_int(instance2)
+    assert(instance1 == instance2)
+
+
+def assert_float_equal(instance1, instance1):
+    assert_is_float(instance1)
+    assert_is_float(instance2)
+    assert(instance1 == instance2)
+
+
+def assert_str_equal(instance1, instance2):
+    assert_is_str(instance1)
+    assert_is_str(instance2)
+    assert(instance1 == instance2)
+
+
+def assert_bool_equal(instance1, instance2):
+    assert_is_bool(instance1)
+    assert_is_bool(instance2)
+    assert(instance1 == instance2)
+
 # Exceptions
+
+
 class StorgeError(Exception):
 
     def __init__(self, value='A storage error'):
